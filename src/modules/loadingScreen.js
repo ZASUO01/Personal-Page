3a987ed95bdb4f27b9b488f9b35b0c48bb296240
ZASUO01 <souzadraganeelc@gmail.com>
2021-05-05 16:55:49 -0300

    loading bug fixed

--- src/modules/loadingScreen.js
+++ src/modules/loadingScreen.js
@@ -6,10 +6,9 @@ const sleep = (timeMs) => {
 }
 
 const closeWindow = async () => {
-    body.style.overflow = 'hidden';
     await sleep(3000);
     loadingDiv.style.opacity = '0';
-    body.style.overflow = 'initial';
+    body.style.overflow = 'visible';
     await sleep(1000);
     loadingDiv.style.display = 'none';
 }
